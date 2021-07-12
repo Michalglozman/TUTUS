4e6dadfb9a6011811f873c2a3fddbc2ebc358885
michal_glozman <glozmanmichal@gmail.com>
2021-07-12 07:18:00 +0300

    chge5

--- mainObject.php
+++ mainObject.php
@@ -54,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <script src="js\scripts.js"></script>
         <link rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&family=Roboto:wght@100&display=swap" rel="stylesheet">
